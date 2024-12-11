46e76585fc5f396a8bf56db333b9f117b7b2b186
Max A. <maximiliana28@gmail.com>
2024-12-11 15:55:45 +0100

    Solved Day 11

--- Day 11/stones.py
+++ Day 11/stones.py
@@ -1,34 +1,52 @@
-import sys
-sys.set_int_max_str_digits(10000000)
+def one_two_star(stones, iterations):
+    d = {i: 1 for i in stones}
+    
+    for _ in range(iterations):
+        
+        new_dict = {}
+        for key, value in d.items():
+            if key == 0:
+                if not new_dict.get(1):
+                    new_dict[1] = value
+                else:
+                    new_dict[1] += value
+            
+            elif len(str(key)) % 2 == 0:
+                mid = len(str(key)) // 2
+                part1 = int(str(key)[:mid])
+                part2 = int(str(key)[mid:])
+
+                if not new_dict.get(part1):
+                    new_dict[part1] = value
+                else:
+                    new_dict[part1] += value
+
+                if not new_dict.get(part2):
+                    new_dict[part2] = value
+                else:
+                    new_dict[part2] += value
+
+            else:
+                new_val = key * 2024
+                if not new_dict.get(new_val):
+                    new_dict[new_val] = value
+                else:
+                    new_dict[new_val] += value
+        
+        d = new_dict
+    
+    return sum(value for _, value in d.items())
 
 def main():
     with open("../inputs/input11.txt", 'r') as f:
         input = f.readlines()
 
-    l = [i for i in input[0].strip().split()]
-    print(l)
+    l = [int(i) for i in input[0].strip().split()]
 
-    for it in range(25):
-        print(f"Iteration {it}")
-        for i, el in enumerate(l):
-            cp = l[:]
+    
+    print("⭐:", one_two_star(l, 25))
+    print("⭐⭐", one_two_star(l, 75))
 
-            if el == 0:
-                cp[i] = str(1)
-            
-            elif len(el) % 2 == 0:
-                part1 = el[:len(el)//2]
-                part2 = el[len(el)//2]
-
-                cp[i] = part2
-                cp.insert(i, part1)
-            
-            else:
-                cp[i] = str(int(cp[i] * 2024))
-            
-            l = cp
-        print(len(l))
-    print(len(l))
 
 if __name__ == "__main__":
     main()
